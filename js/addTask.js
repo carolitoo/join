32d474f725fa2115187b11dd2d69c4347ff13a32
NanaBa <you@example.com>
2024-01-20 19:49:30 +0100

    add task updatet

---
+++ js/addTask.js
@@ -0,0 +1,8 @@
+/**
+ * This function changes the colors of the prioritybuttons depending on wich is activ 
+ */
+function changeButtonColorsUrgent(){ 
+    document.getElementById('urgentButton').style.backgroundColor = "#FF3D00";
+    document.getElementById('urgentButton').classList.remove("box-shadow");
+
+}
--- js/summary.js
+++
@@ -1,4 +0,0 @@
-function hoverToDoBox() {
-    document.getElementById('toDoAndDoneBox').style.backgroundColor = "#2A3647";
-    document.getElementById('head-img').src = 'img/star.jpg';
-}
