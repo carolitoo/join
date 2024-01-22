9a88efc01d3a87c9c102e851fa9a19b079c6f80c
NanaBa <you@example.com>
2024-01-22 19:12:42 +0100

    add task prio buttons colors

--- js/addTask.js
+++ js/addTask.js
@@ -1,8 +1,50 @@
 /**
- * This function changes the colors of the prioritybuttons depending on wich is activ 
+ * This function changes the colors of the priority buttons and changes the remaining two buttons back, if switched between them
  */
 function changeButtonColorsUrgent(){ 
-    document.getElementById('urgentButton').style.backgroundColor = "#FF3D00";
-    document.getElementById('urgentButton').classList.remove("box-shadow");
+    document.getElementById('urgentButton').classList.add("urgent-btn-red");
+    document.getElementById('urgentButton').classList.remove("prio-btn-neutral");
+    document.getElementById('urgent-icon').src = './assets/img/Prio alta w.svg';
 
+    document.getElementById('mediumButton').classList.remove("medium-btn-yellow"); 
+    document.getElementById('mediumButton').classList.add("prio-btn-neutral");
+    document.getElementById('medium-icon').src = './assets/img/prio media y.svg';
+
+    document.getElementById('lowButton').classList.remove("low-btn-green");
+    document.getElementById('lowButton').classList.add("prio-btn-neutral");
+    document.getElementById('low-icon').src = './assets/img/Prio baja.svg';
+}
+
+/**
+ * This function changes the colors of the priority buttons and changes the remaining two buttons back, if switched between them
+ */
+function changeButtonColorsMedium(){ 
+    document.getElementById('mediumButton').classList.add("medium-btn-yellow");
+    document.getElementById('mediumButton').classList.remove("prio-btn-neutral");
+    document.getElementById('medium-icon').src = './assets/img/capa 2.svg';
+
+    document.getElementById('urgentButton').classList.remove("urgent-btn-red");
+    document.getElementById('urgentButton').classList.add("prio-btn-neutral");
+    document.getElementById('urgent-icon').src = './assets/img/Prio alta2.svg';
+
+    document.getElementById('lowButton').classList.remove("low-btn-green");
+    document.getElementById('lowButton').classList.add("prio-btn-neutral");
+    document.getElementById('low-icon').src = './assets/img/Prio baja.svg';
+}
+
+/**
+ * This function changes the colors of the priority buttons and changes the remaining two buttons back, if switched between them
+ */
+function changeButtonColorsLow(){ 
+    document.getElementById('lowButton').classList.add("low-btn-green");
+    document.getElementById('lowButton').classList.remove("prio-btn-neutral");
+    document.getElementById('low-icon').src = './assets/img/Prio baja w.svg';
+
+    document.getElementById('mediumButton').classList.remove("medium-btn-yellow");
+    document.getElementById('mediumButton').classList.add("prio-btn-neutral");
+    document.getElementById('medium-icon').src = './assets/img/prio media y.svg';
+
+    document.getElementById('urgentButton').classList.remove("urgent-btn-red");
+    document.getElementById('urgentButton').classList.add("prio-btn-neutral");
+    document.getElementById('urgent-icon').src = './assets/img/Prio alta2.svg';
 }
